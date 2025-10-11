cec8ab2ef2c0a338a4975848a0aadf988fed2f66
liujip0 <liujip2020@gmail.com>
2025-10-11 02:33:00 -0400

    add wraparound auto i think

--- src/main/java/frc/robot/commands/autos/Autos.java
+++ src/main/java/frc/robot/commands/autos/Autos.java
@@ -7,6 +7,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.commands.AutoIntakePower;
+import frc.robot.commands.AutoPickupCoral;
+import frc.robot.commands.IntakeGroundCoral;
 import frc.robot.subsystems.GroundIntake;
 import frc.robot.subsystems.SuperStructure.Arm;
 import frc.robot.subsystems.SuperStructure.Elevator;
@@ -21,7 +23,11 @@ public class Autos {
   private GroundIntake groundIntake;
   private Drive drive;
 
-  private SuperstructureCommands SuperstructureCommands = new SuperstructureCommands();
+  private GroundIntakeCommands groundIntakeCommands = new GroundIntakeCommands();
+  private CoralArmCommands coralArmCommands = new CoralArmCommands();
+  private AlgaeArmCommands algaeArmCommands = new AlgaeArmCommands();
+  private IntakeCommands intakeCommands = new IntakeCommands();
+
   public AutoRoutines AutoRoutines = new AutoRoutines();
 
   public Autos(
@@ -45,35 +51,58 @@ public class Autos {
 
       return Commands.sequence(
           autoFactory.resetOdometry(trajectoryName, 0),
-          new AutoIntakePower(intake, -1),
+          intakeCommands.coralIntake(),
 
-          // Go to first piece
+          // Place first coral (preload)
           Commands.parallel(
               autoFactory.trajectoryCmd(trajectoryName, 0),
+              Commands.sequence(groundIntakeCommands.freeArm(), coralArmCommands.L2Coral())),
+          intakeCommands.coralOuttake(),
+          new WaitCommand(0.5),
+
+          // Pluck algae
+          Commands.parallel(
+              autoFactory.trajectoryCmd(trajectoryName, 1),
               Commands.sequence(
-                  SuperstructureCommands.freeArm(),
-                  new InstantCommand(
-                      () -> {
-                        arm.setTargetAngle(Constants.Presets.armSafePosition, 0);
-                      }))),
-          new AutoIntakePower(intake, 0.75),
+                  intakeCommands.stopIntake(),
+                  algaeArmCommands.L3Algae(),
+                  new WaitCommand(0.25),
+                  intakeCommands.algaeIntake(),
+                  groundIntakeCommands.groundIntakeDown())),
           new WaitCommand(0.5),
-          autoFactory.trajectoryCmd(trajectoryName, 1),
-          new WaitCommand(0.5),
-          autoFactory.trajectoryCmd(trajectoryName, 2),
-          // new AutoPickupCoral(drive, groundIntake, arm, elevator, intake, 2),
-          // new InstantCommand(
-          //     () -> {
-          //       arm.setTargetAngle(Constants.Presets.armSafePosition, 0);
-          //     }),
-          new WaitCommand(0.5),
-          autoFactory.trajectoryCmd(trajectoryName, 3),
-          new WaitCommand(0.5),
-          autoFactory.trajectoryCmd(trajectoryName, 4));
+
+          // Pick up second coral
+          Commands.parallel(
+              autoFactory.trajectoryCmd(trajectoryName, 2),
+              Commands.sequence(
+                  new WaitCommand(0.55), intakeCommands.coralIntake(), new WaitCommand(0.5))),
+          groundIntakeCommands.visionCoralPickup(),
+
+          // Place second coral
+          Commands.parallel(
+              autoFactory.trajectoryCmd(trajectoryName, 3),
+              Commands.sequence(new WaitCommand(1), coralArmCommands.L3Coral())),
+          intakeCommands.coralOuttake(),
+          new WaitCommand(0.2),
+
+          // Pick up third coral
+          Commands.parallel(
+              autoFactory.trajectoryCmd(trajectoryName, 4),
+              Commands.sequence(
+                  new WaitCommand(0.2),
+                  intakeCommands.stopIntake(),
+                  new IntakeGroundCoral(groundIntake, arm, elevator, intake))),
+          groundIntakeCommands.visionCoralPickup(),
+
+          // Place third coral
+          Commands.parallel(
+              autoFactory.trajectoryCmd(trajectoryName, 5),
+              Commands.sequence(new WaitCommand(0.5), coralArmCommands.L3Coral())),
+          intakeCommands.coralOuttake());
     }
   }
 
-  private class SuperstructureCommands {
+  private class GroundIntakeCommands {
     private Command freeArm() {
       return Commands.sequence(
           new InstantCommand(
@@ -87,12 +116,61 @@ public class Autos {
               }));
     }
 
-    private Command L2Outtake() {
+    private Command groundIntakeDown() {
+      return new InstantCommand(
+          () -> {
+            groundIntake.setIntakePosition(Constants.Presets.groundIntakeHover);
+          });
+    }
+
+    private Command visionCoralPickup() {
+      return new AutoPickupCoral(drive, groundIntake, arm, elevator, intake, 2);
+    }
+  }
+
+  private class CoralArmCommands {
+    private Command L2Coral() {
       return new InstantCommand(
           () -> {
             elevator.setElevatorPosition(Constants.Presets.liftOuttakeL2);
             arm.setTargetAngle(Constants.Presets.armOuttakeL2, 0);
           });
+    }
+
+    private Command L3Coral() {
+      return new InstantCommand(
+          () -> {
+            elevator.setElevatorPosition(Constants.Presets.liftOuttakeL3);
+            arm.setTargetAngle(Constants.Presets.armOuttakeL3, 0);
+          });
+    }
+  }
+
+  private class AlgaeArmCommands {
+    private Command L3Algae() {
+      return new InstantCommand(
+          () -> {
+            elevator.setElevatorPosition(Constants.Presets.liftAlgeaL3);
+            arm.setTargetAngle(Constants.Presets.armAlgeaL3, 0);
+          });
+    }
+  }
+
+  private class IntakeCommands {
+    private Command coralIntake() {
+      return new AutoIntakePower(intake, -1);
+    }
+
+    private Command coralOuttake() {
+      return new AutoIntakePower(intake, 0.75);
+    }
+
+    private Command algaeIntake() {
+      return new AutoIntakePower(intake, 1);
+    }
+
+    private Command stopIntake() {
+      return new AutoIntakePower(intake, 0);
     }
   }
 }
