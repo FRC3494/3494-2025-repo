41d86cdff44dc1909ed6870b72a630cc6bab48f0
liujip0 <liujip2020@gmail.com>
2025-10-19 07:18:09 -0400

    arm zero

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -150,7 +150,7 @@ public final class Constants {
     public static double manualPowerPOS = 0.006;
     public static double normalPIDRange = 0.9;
 
-    public static double absoluteEncoderOffset = 0.8282509;
+    public static double absoluteEncoderOffset = 0.8301311;
 
     public static int normalCurrentLimit = 75;
 
